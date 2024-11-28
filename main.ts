cb0fc1e8515cbd4b57771a59d1f1f204ef172548
Daniel Salazar Pares <146827286+Daniel-Salazar-Pares@users.noreply.github.com>
2024-11-28 16:59:01 +0100

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
- 
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (demon.vy == 0) {
+        demon.vy = -200
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleBlueCrystal, function (sprite, location) {
+    game.gameOver(true)
+    game.setGameOverEffect(true, effects.confetti)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleRedCrystal, function (sprite, location) {
+    game.gameOver(false)
+    game.setGameOverEffect(false, effects.melt)
+})
+let demon: Sprite = null
+scene.setBackgroundColor(9)
+demon = sprites.create(assets.image`LilDemon`, SpriteKind.Player)
+scene.cameraFollowSprite(demon)
+controller.moveSprite(demon, 100, 0)
+tiles.setCurrentTilemap(tilemap`level0`)
+demon.ay = 500
