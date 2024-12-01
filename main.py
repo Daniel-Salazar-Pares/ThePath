bc6ebe633c061545658bf2adacc61635e1867916
AlejandroCRG1 <146829942+AlejandroCRG1@users.noreply.github.com>
2024-12-01 19:17:54 +0100

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, main.py

--- main.py
+++ main.py
@@ -1,22 +1,97 @@
-def on_overlap_tile(sprite2, location2):
+@namespace
+class SpriteKind:
+    Abrusto = SpriteKind.create()
+
+def on_overlap_tile(sprite3, location):
+    game.game_over(True)
+    game.set_game_over_effect(True, effects.confetti)
+scene.on_overlap_tile(SpriteKind.player,
+    sprites.dungeon.collectible_blue_crystal,
+    on_overlap_tile)
+
+def on_overlap_tile2(sprite2, location2):
     game.game_over(False)
     game.set_game_over_effect(False, effects.melt)
 scene.on_overlap_tile(SpriteKind.player,
     sprites.dungeon.collectible_red_crystal,
-    on_overlap_tile)
+    on_overlap_tile2)
 
 def on_a_pressed():
     if demon.x == 0:
         demon.vy = 5
 controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
 
-def on_overlap_tile2(sprite, location):
-    game.game_over(True)
-    game.set_game_over_effect(True, effects.confetti)
-scene.on_overlap_tile(SpriteKind.player,
-    sprites.dungeon.collectible_blue_crystal,
-    on_overlap_tile2)
+def on_on_overlap(sprite, otherSprite):
+    global bee
+    sprites.destroy(otherSprite)
+    bee = sprites.create(img("""
+            . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . f f f f f f f . . . . . 
+                    . . . f 1 1 1 f 1 1 1 f . . . . 
+                    . . . f 1 1 1 1 1 1 1 f . . . . 
+                    . . . . . 1 1 f 1 1 . . . . . . 
+                    . . . . f f f f f f f . . . . . 
+                    . . . f 5 5 5 5 5 5 5 f . . . . 
+                    . . . f f 5 5 5 5 5 f f . . . . 
+                    . . . f 5 5 5 5 5 5 5 f . . . . 
+                    . . . . f f f f f f f . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . .
+        """),
+        SpriteKind.enemy)
+    animation.run_image_animation(bee,
+        [img("""
+                . . . . . . . . . . . . . . . . 
+                        . . . . . . . . . . . . . . . . 
+                        . . . . f f f f f f f . . . . . 
+                        . . . f 1 1 1 f 1 1 1 f . . . . 
+                        . . . f 1 1 1 1 1 1 1 f . . . . 
+                        . . . . . 1 1 f 1 1 . . . . . . 
+                        . . . . f f f f f f f . . . . . 
+                        . . . f 5 5 5 5 5 5 5 f . . . . 
+                        . . . f f 5 5 5 5 5 f f . . . . 
+                        . . . f 5 5 5 5 5 5 5 f . . . . 
+                        . . . . f f f f f f f . . . . . 
+                        . . . . . . . . . . . . . . . . 
+                        . . . . . . . . . . . . . . . . 
+                        . . . . . . . . . . . . . . . . 
+                        . . . . . . . . . . . . . . . . 
+                        . . . . . . . . . . . . . . . .
+            """),
+            img("""
+                . . . . . . . . . . . . . . . . 
+                        . . . . . . . . . . . . . . . . 
+                        . . . . . . . . . . . . . . . . 
+                        . . . . . . . . . . . . . . . . 
+                        . . . . . . . . . . . . . . . . 
+                        . . . . . . . . . . . . . . . . 
+                        . . . . f f f f f f f . . . . . 
+                        . . . f 5 5 5 5 5 5 5 f . . . . 
+                        . . . f f 5 5 5 5 5 f f . . . . 
+                        . . . f 5 5 5 5 5 5 5 f . . . . 
+                        . . . . f f f f f f f . . . . . 
+                        . . . . . . . . . . . . . . . . 
+                        . . . . . . . . . . . . . . . . 
+                        . . . . . . . . . . . . . . . . 
+                        . . . . . . . . . . . . . . . . 
+                        . . . . . . . . . . . . . . . .
+            """)],
+        100,
+        True)
+    bee.set_position(demon.x - 80, demon.y - 80)
+    bee.follow(demon)
+sprites.on_overlap(SpriteKind.player, SpriteKind.Abrusto, on_on_overlap)
 
+def on_on_overlap2(sprite4, otherSprite2):
+    info.change_life_by(-1)
+sprites.on_overlap(SpriteKind.player, SpriteKind.enemy, on_on_overlap2)
+
+bee: Sprite = None
+abrusto: Sprite = None
 demon: Sprite = None
 scene.set_background_color(9)
 demon = sprites.create(assets.image("""
@@ -25,9 +100,35 @@ demon = sprites.create(assets.image("""
 scene.camera_follow_sprite(demon)
 controller.move_sprite(demon, 100, 0)
 tiles.set_current_tilemap(tilemap("""
-    level0
+    level3
 """))
 demon.ay = 500
+for value in tiles.get_tiles_by_type(assets.tile("""
+    myTile2
+""")):
+    abrusto = sprites.create(img("""
+            . . . . . f f f f f f . . . . . 
+                    . . . f f 6 6 6 6 6 6 f f . . . 
+                    . . f 6 6 6 6 c c c c 6 6 f . . 
+                    . f 6 6 c 6 6 c c 6 6 c 6 6 f . 
+                    . f 6 6 c c c c c 6 6 c c 6 f . 
+                    f 6 c c c c c c c c c c c c 6 f 
+                    f 6 6 6 c c c c c c c c c 6 6 f 
+                    f 6 6 6 c c c c c c c c c c 6 f 
+                    f 6 c c c 6 c c c c 6 6 c c 6 f 
+                    f 6 c c c c c c c c 6 6 c c 6 f 
+                    f 6 c c c c c c f f c c c c 6 f 
+                    f f c c f f f c f f c f f c f f 
+                    f f c c f f f c c c c f f c f f 
+                    f f f c f f f c c f c c c f f f 
+                    . f f f f f f f f f f f f f f . 
+                    . . f f f f f f f f f f f f . .
+        """),
+        SpriteKind.Abrusto)
+    tiles.place_on_tile(abrusto, value)
+    tiles.set_tile_at(value, assets.tile("""
+        transparency16
+    """))
 
 def on_on_update():
     demon.set_image(assets.image("""
--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+namespace SpriteKind {
+    export const Abrusto = SpriteKind.create()
+}
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleBlueCrystal, function (sprite3, location) {
+    game.gameOver(true)
+    game.setGameOverEffect(true, effects.confetti)
+})
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleRedCrystal, function (sprite2, location2) {
     game.gameOver(false)
     game.setGameOverEffect(false, effects.melt)
@@ -7,17 +14,103 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         demon.vy = 5
     }
 })
-scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleBlueCrystal, function (sprite, location) {
-    game.gameOver(true)
-    game.setGameOverEffect(true, effects.confetti)
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Abrusto, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    bee = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . f 1 1 1 f 1 1 1 f . . . . 
+        . . . f 1 1 1 1 1 1 1 f . . . . 
+        . . . . . 1 1 f 1 1 . . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . f 5 5 5 5 5 5 5 f . . . . 
+        . . . f f 5 5 5 5 5 f f . . . . 
+        . . . f 5 5 5 5 5 5 5 f . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    animation.runImageAnimation(
+    bee,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . f 1 1 1 f 1 1 1 f . . . . 
+        . . . f 1 1 1 1 1 1 1 f . . . . 
+        . . . . . 1 1 f 1 1 . . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . f 5 5 5 5 5 5 5 f . . . . 
+        . . . f f 5 5 5 5 5 f f . . . . 
+        . . . f 5 5 5 5 5 5 5 f . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . f 5 5 5 5 5 5 5 f . . . . 
+        . . . f f 5 5 5 5 5 f f . . . . 
+        . . . f 5 5 5 5 5 5 5 f . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `],
+    100,
+    true
+    )
+    bee.setPosition(demon.x - 80, demon.y - 80)
+    bee.follow(demon)
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+})
+let bee: Sprite = null
+let abrusto: Sprite = null
 let demon: Sprite = null
 scene.setBackgroundColor(9)
 demon = sprites.create(assets.image`LilDemon`, SpriteKind.Player)
 scene.cameraFollowSprite(demon)
 controller.moveSprite(demon, 100, 0)
-tiles.setCurrentTilemap(tilemap`level0`)
+tiles.setCurrentTilemap(tilemap`level3`)
 demon.ay = 500
+for (let value of tiles.getTilesByType(assets.tile`myTile2`)) {
+    abrusto = sprites.create(img`
+        . . . . . f f f f f f . . . . . 
+        . . . f f 6 6 6 6 6 6 f f . . . 
+        . . f 6 6 6 6 c c c c 6 6 f . . 
+        . f 6 6 c 6 6 c c 6 6 c 6 6 f . 
+        . f 6 6 c c c c c 6 6 c c 6 f . 
+        f 6 c c c c c c c c c c c c 6 f 
+        f 6 6 6 c c c c c c c c c 6 6 f 
+        f 6 6 6 c c c c c c c c c c 6 f 
+        f 6 c c c 6 c c c c 6 6 c c 6 f 
+        f 6 c c c c c c c c 6 6 c c 6 f 
+        f 6 c c c c c c f f c c c c 6 f 
+        f f c c f f f c f f c f f c f f 
+        f f c c f f f c c c c f f c f f 
+        f f f c f f f c c f c c c f f f 
+        . f f f f f f f f f f f f f f . 
+        . . f f f f f f f f f f f f . . 
+        `, SpriteKind.Abrusto)
+    tiles.placeOnTile(abrusto, value)
+    tiles.setTileAt(value, assets.tile`transparency16`)
+}
 game.onUpdate(function () {
     demon.setImage(assets.image`LilDemon`)
     if (demon.vy < 0) {
